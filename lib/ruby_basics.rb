9c44126e60eed93841c3da98993148e912475b7a
wei feng <theweifeng@gmail.com>
2019-04-09 21:47:38 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,13 +15,13 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+"expert"
 end
 
-def pizza_party
-
+def pizza_party(val = "cheese")
+  val
 end
